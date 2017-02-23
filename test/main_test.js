96072a090c92b8bb8e62a36ac15feb0d6bf0ec15
Chris Skinner <c.skinner@blueyonder.co.uk>
2017-02-23 20:45:01 +0100

    Remove base test file that confirms Mocha & Chai are working

--- test/main_test.js
+++
@@ -1,9 +0,0 @@
-var expect = require('chai').expect;
-
-// Test suite
-describe('Mocha', function(){
-	// test spec (unit test)
-	it('should run our test using npm', function(){
-		expect(true).to.be.ok;
-	});
-});
